e9fd5789390ad0269133587476a623a67714ff7e
FayeRingo <lscd613@gmail.com>
2025-03-19 18:36:47 +0800

    Delete HFTradingSystem directory

--- HFTradingSystem/HFTradingSystem/main.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#define _CRT_SECURE_NO_WARNINGS
-#pragma comment(lib,"./ThostTraderApi/thostmduserapi_se.lib")
-#pragma comment(lib,"./ThostTraderApi/thosttraderapi_se.lib")
--- HFTradingSystem/HFTradingSystem/mystruct.cpp
+++
@@ -1,9 +0,0 @@
-#include "mystruct.h"
-
-void ReadKbarSeries(std::string fileName, std::vector<Kbar>& kbar_vec)
-{
-}
-
-void Save_FileName(std::string path, std::vector<std::string>& fileName_vec)
-{
-}
